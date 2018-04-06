d47b2fdc592c6c56a119ab19795819df203ea2b7
Aaron-SP <springstrohaa@gmail.com>
2018-04-05 22:05:12 -0400

    WEB: Added salt water mixing calculator

---
+++ salt/js.js
@@ -0,0 +1,31 @@
+function calculate() {
+    // Get inputs
+    var in_sg = Number(document.getElementById("in_sg").value);
+    var in_vol_gal = Number(document.getElementById("in_vol_gal").value);
+    var in_vol_m3 = in_vol_gal / 264.172;
+    var in_temp_degC = 25.0;
+
+    // Calculate regression constants
+    var in_temp_K = in_temp_degC + 273.15;
+    var in_inv_temp_K = 1.0 / in_temp_K;
+    var in_temp_K2 = in_temp_K * in_temp_K;
+    var in_temp_K3 = in_temp_K2 * in_temp_K;
+
+    // Calculate weight of solution
+    var water_density_kg_m3 = rho_water(in_inv_temp_K, in_temp_K, in_temp_K2, in_temp_K3);
+    var sample_density_kg_m3 = in_sg * water_density_kg_m3;
+    var salt_kg = (sample_density_kg_m3 - water_density_kg_m3) * in_vol_m3;
+    var salt_g = salt_kg * 1000.0;
+    var salt_ml = salt_g / 2.16; // g / (cm^3=ml)
+    var salt_tbsp = salt_ml * 0.067628;
+    var salt_cup = salt_tbsp * 0.0625;
+
+    // Set output values
+    document.getElementById("out_salt_g").value = salt_g.toFixed(2);
+    document.getElementById("out_salt_ml").value = salt_ml.toFixed(2);
+    document.getElementById("out_salt_tbsp").value = salt_tbsp.toFixed(2);
+    document.getElementById("out_salt_cup").value = salt_cup.toFixed(2);
+
+    // Return no form action
+    return false;
+}
