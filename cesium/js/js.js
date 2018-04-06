45130baa8075c5a69789c352662270ce4cc175ef
Aaron-SP <springstrohaa@gmail.com>
2018-04-06 11:26:34 -0400

    WEB: gutted react in cesium and fixed page load

---
+++ cesium/js/js.js
@@ -0,0 +1,5 @@
+let viewer = new Cesium.Viewer('cesiumContainer');
+
+// Turn off loading indicator
+let loadingIndicator = document.getElementById('loadingIndicator');
+loadingIndicator.style.display = 'none';
