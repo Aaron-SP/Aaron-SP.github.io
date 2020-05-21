ed5a0649988bc7b2f84f02907984eea5e6b80f2e
Aaron-SP <springstrohaa@gmail.com>
2020-05-21 13:01:21 -0500

    Resize canvas at nebula start

--- nebula/web.js
+++ nebula/web.js
@@ -8,15 +8,15 @@ function context_on_init_js(context_ptr) {
 
     // Initialize the text editor
     document.getElementById("tool_shader_text").value = Module.get_fragment_source();
+
+    // Trigger window resize
+    window.dispatchEvent(new Event('resize'));
 }
 
 // This function is called every frame
 function context_on_frame_js(context_ptr) {
     // Save the frame pointer
     last_context_ptr = context_ptr;
-
-    // Print frame pointer
-    console.log(context_ptr);
 }
 
 function update_nebula_shader() {
